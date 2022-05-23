28362ec4146d6a91635fcdf89903bc4da59a7e48
Duda <Duda>
2022-05-23 20:43:51 -0300

    atualizacoes para subir no github nem sabia que ja tinha pasta git aqui

---
+++ src/pages/Home/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from "axios";
+import * as S from './styled';
+import { useNavigate } from 'react-router-dom'; // professor usou UseHistory que não se usa mais
+
+export default function App(props) {
+  const navigate = useNavigate();
+  const [ usuario, setUsuario ] = useState(''); //useState para citar e modificar estados
+  const [ erro, setErro ] = useState(false);
+
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+      const repositories = response.data;
+      const repositoriesName = [];
+      repositories.map((repository) => {
+        repositoriesName.push(repository.name);
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      setErro(false);
+      navigate('/repositories');
+    })
+    .catch(err => {
+      setErro(true);
+    });
+  }
+    
+  return (
+    <S.HomeContainer>
+      <S.Content>
+        <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
+        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+        { erro ? <S.ErrorMsg>Ocorreu um erro. Tente novamente.</S.ErrorMsg> : ''}
+      </S.Content>
+    </S.HomeContainer>
+    );
+}
+
